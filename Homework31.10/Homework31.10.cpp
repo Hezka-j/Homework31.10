ad893f1bb8e40a7b1184714fa2f60edd6200011a
Hezka-j <145163687+Hezka-j@users.noreply.github.com>
2023-11-01 21:46:42 +0200

    Add files via upload

---
+++ Homework31.10/Homework31.10.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int m;
+    int n;
+    int* pm = &m;
+    int* pn = &n;
+    
+    cout << pm << "\n";
+    cout << pn << "\n";
+    
+    
+    cout << "Int count definition: " << (pn - pm) / sizeof(int) << "\n";
+    cout << "Int definition: " << pn - pm << "\n";
+    cout << "Byte defenition: " << (pn - pm) * sizeof(int) << "\n";
+}
+
